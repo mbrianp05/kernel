e48e08617f49a2bf07dd0c88bb919df07215823d
Brian <mbrianp05@gmail.com>
2021-02-23 19:04:03 +0100

    Correcting assets

--- lib/Mbrianp/Kernel/templates/DefaultHomepage.html.php
+++ lib/Mbrianp/Kernel/templates/DefaultHomepage.html.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="public/kernel/assets/css/all.min.css">
+    <link rel="stylesheet" href="kernel/assets/css/all.min.css">
     <style>
         body, html {
             width: 100%;
